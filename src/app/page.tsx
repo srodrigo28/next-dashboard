60e63521f79ef74857917a1cb0d283b2149603b8
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2024-10-31 11:03:58 -0300

    updated-env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
 const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
 
 type TypeGestor = {
