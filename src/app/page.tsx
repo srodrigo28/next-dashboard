d27dc8196ac3bf04b2e1b4d5826910861138b958
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2024-10-31 10:48:58 -0300

    env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
 
+type Igestor {
+  id: string | number
+}
+
 export default function Home() {
 
   const [nome, setNome] = useState('');
